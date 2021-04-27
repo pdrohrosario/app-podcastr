5ecd9aca16c191eb282ed12db7576e20d152037c
Pedro Rosário <pdrorosa18912@gmail.com>
2021-04-27 20:43:31 -0300

    Reformulando a entrada e formatação de dados da api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,28 @@
 ///SSG - Server Side Generetion
 
-export default function Home(props) {
+import{GetStaticProps} from 'next';
+import { api } from '../services/api';
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+
+type Episode = {
+  id:string;
+  title:string;
+  thumbnail:string;
+  description:string;
+  members:string;
+  duration:number;
+  durationAsString:string;
+  url:string;
+  publishedAt:string;
+}
+
+type HomeProps = {
+  episodes:Episode[];
+}
+
+export default function Home(props: HomeProps) {
   return (
     <div>
       <h1>Index</h1>
@@ -9,13 +31,34 @@ export default function Home(props) {
   )
 }
 
-export async function getStaticProps(){
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
+//get data api
+export const getStaticProps: GetStaticProps = async() => {
+  const { data } = await api.get('episodes', {
+    params:{
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  //data format
+  const episodes = data.map(episode => {
+    return {
+      id:episode.id,
+      title:episode.title,
+      thumbnail:episode.thumbnail,
+      members:episode.members,
+      publishedAt:format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR}),
+      duration: Number(episode.file.duration),
+      durationAsString:convertDurationToTimeString(Number(episode.file.duration)),
+      description:episode.description,
+      url:episode.file.url,
+    };
+  });
 
   return{
     props:{
-      episodes:data,
+      episodes:episodes,
     },
     revalidate: 60 * 60 * 8,
   }
