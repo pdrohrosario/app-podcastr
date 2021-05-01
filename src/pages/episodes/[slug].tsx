7c413da722e05fb608b53016d3314a8507abafba
Pedro Rosário <pdrorosa18912@gmail.com>
2021-04-30 22:48:41 -0300

    Criando redirecionamento rotas e também criando a página dos episódios.

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,10 +1,86 @@
+import { GetStaticProps, GetStaticPaths } from 'next';
 import {useRouter} from 'next/router';
+import { api } from '../../services/api';
+import{ format, parseISO} from 'date-fns';
+import Link from 'next/link';
+import Image from 'next/image';
 
-export default function Episode(){
+import ptBR from 'date-fns/locale/pt-BR';
+import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 
-    const router = useRouter();
+import styles from './episode.module.scss';
 
+type Episode = {
+    id:string;
+    title:string;
+    thumbnail:string;
+    members:string;
+    duration:number;
+    durationAsString:string;
+    url:string;
+    publishedAt:string;
+    description:string;
+}
+
+type EpisodeProps = {
+    episode:Episode;
+}
+
+export default function Episode({episode}: EpisodeProps) {
     return(
-        <h1>Hello World!</h1>
+      <div className={styles.episode}>
+          <div className={styles.thumbnailContainer}>
+           <Link href={`/`}>
+                <button>
+                    <img src="/arrow-left.svg" alt="Voltar" />
+                </button>
+            </Link>
+            <Image width={700} height={160} src={episode.thumbnail} objectFit="cover" />
+            <button>
+               <img src="/play.svg" alt="Tocar episódio" />
+            </button>
+          </div>
+
+          <header>
+            <h1>{episode.title}</h1>
+            <span>{episode.members}</span>
+            <span>{episode.publishedAt}</span>
+            <span>{episode.durationAsString}</span>
+          </header>
+
+          <div className={styles.description} dangerouslySetInnerHTML={{__html:episode.description}} />
+      </div>
     )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    return{
+        paths:[],
+        fallback:'blocking',
+    }
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+    const {slug} = context.params; //Pega o ID do ep pelo contexto 
+
+    const {data} = await api.get(`/episodes/${slug}`)    
+
+    const episode = {
+        id:data.id,
+        title:data.title,
+        thumbnail:data.thumbnail,
+        members:data.members,
+        publishedAt:format(parseISO(data.published_at), 'd MMM yy', {locale: ptBR}),
+        duration: Number(data.file.duration),
+        durationAsString:convertDurationToTimeString(Number(data.file.duration)),
+        description:data.description,
+        url:data.file.url,
+    };
+
+    return {
+        props: {
+            episode,
+        },
+        revalidate:60 * 60 *24, //24 horas
+    }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,6 @@ type Episode = {
   id:string;
   title:string;
   thumbnail:string;
-  description:string;
   members:string;
   duration:number;
   durationAsString:string;
@@ -128,7 +127,6 @@ export const getStaticProps: GetStaticProps = async() => {
       publishedAt:format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR}),
       duration: Number(episode.file.duration),
       durationAsString:convertDurationToTimeString(Number(episode.file.duration)),
-      description:episode.description,
       url:episode.file.url,
     };
   });
