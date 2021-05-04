efd5113b218ea039a312fd3c4ab3e01d6c3f1ca0
Pedro Rosário <pdrorosa18912@gmail.com>
2021-05-03 21:53:11 -0300

    Criando o context do player e exibindo informação nele.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,14 @@
 
 import{GetStaticProps} from 'next';
 import Image from 'next/image';
-import { api } from '../services/api';
 import Link from 'next/link';
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import { useContext } from 'react';
+
+import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+import { PlayerContext } from '../contexts/PlayerContext';
 
 import styles from './home.module.scss';
 
@@ -27,6 +30,8 @@ type HomeProps = {
 }
 
 export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
+  const {play} = useContext(PlayerContext);
+
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
@@ -47,7 +52,7 @@ export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button">
+                <button type="button"  onClick={() => play(episode)}>
                   <img src="/play-green.svg" alt="Tocar episódio"/>
                 </button>               
               </li>
